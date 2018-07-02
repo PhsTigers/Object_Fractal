4867d59d5112b6806f01fed66caffb171def954a
PhsTigers <mwomack@pcoe.k12.ca.us>
2018-07-02 09:28:17 -0700

    Update branch.js

--- Object_Fractal/branch.js
+++ Object_Fractal/branch.js
@@ -28,6 +28,21 @@ let newbrM=p5.Vector.add(this.end,dir);
 let middle=new branch(this.end,newbrM);
 return middle;
 }
+branchRr(){
+let dir=p5.Vector.sub(this.end,this.begin);
+dir.rotate(PI/3);dir.mult(.677);
+let newbrR=p5.Vector.add(this.end,dir);
+let right=new branch(this.end,newbrR);
+return right;
+}
+
+branchLl(){
+let dir=p5.Vector.sub(this.end,this.begin);
+dir.rotate(-PI/3);dir.mult(.677);
+let newbrL=p5.Vector.add(this.end,dir);
+let left=new branch(this.end,newbrL);
+return left;
+}
 
   
   
